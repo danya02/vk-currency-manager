f7b03c8d042aeab2946ff07eb9054b5ef2aae4e9
Danya Generalov <danya02@me.com>
2019-02-03 14:48:21 +0300

    Adding business with dbase.

--- flask_server.py
+++ flask_server.py
@@ -3,13 +3,48 @@ import traceback
 import vk_api
 from flask import Flask, request
 
+from peewee import *
+import peewee
+
+db = MySQLDatabase('vkfinance', user='vkfinance', password='12349876')
+db.connect()
+
+
+class BaseModel(Model):
+    class Meta:
+        database = db
+
+
+peewee.BaseModel = BaseModel
+#import peeweedbevolve
+
+
+class User(BaseModel):
+    user_id = BigIntegerField()
+    balance = BigIntegerField(default=0)
+
+
+class Chat(BaseModel):
+    chat_id = BigIntegerField()
+
+
+class LocalBalance(BaseModel):
+    user = ForeignKeyField(User, backref='local_balances')
+    balance = BigIntegerField(default=0)
+    chat = ForeignKeyField(Chat, backref='balances')
+
+
+#peeweedbevolve.evolve(db, interactive=False)
+
+db.create_tables([User, Chat, LocalBalance])
+
 app = Flask(__name__)
 
 with open('/var/vk-bots/currency/token.txt') as o:
     token = o.read().strip()
 
 
-@app.route('/endpoint', methods=['POST'])
+@app.route('/endpoint', methods=['GET', 'POST'])
 def main():
     try:
         data = request.get_json(force=True)
@@ -38,5 +73,41 @@ def main():
 
 
 def process_msg(text, user, to):
+    text = text.lower()
     if 'bot' in text:
-        return "Recv'd message from " + str(user) + ", responding to " + str(to) + " re text: '" + text + "'."
+        text = text.split()
+        try:
+            user_obj = User.get(User.user_id == user)
+        except DoesNotExist:
+            user_obj = User.create(user_id=user)
+        if user == to:  # private chat
+            if text[1] == 'balance':
+                return 'Your global balance is: ₲' + str(
+                    user_obj.balance) + '. To see your local balance, repeat the query inside a chat with the ' \
+                                        'community active in the chat. '
+            elif text[1]=='get':
+                with db.atomic():
+                    user_obj.balance+=10
+                    user_obj.save()
+                    return '₲10 GET!'
+        else:  # public chat
+            try:
+                chat = Chat.get(Chat.chat_id == to)
+
+            except DoesNotExist:
+                chat = Chat.create(chat_id=to)
+            try:
+                local_balance = LocalBalance.get(LocalBalance.user == user_obj, LocalBalance.chat == chat)
+
+            except DoesNotExist:
+                local_balance = LocalBalance.create(user=user_obj, chat=chat)
+            if text[1] == 'balance':
+                return 'Your local balance for this chat (id ' + str(chat.chat_id) + ') is: £' + str(
+                    local_balance.balance) + '. To see your global balance, start a private chat with this community.'
+            elif text[1]=='get':
+                with db.atomic():
+                    local_balance.balance+=10
+                    local_balance.save()
+                    return '£10 GET!'
+
+        return 'Request fell through conditions -- that\'s an error.'
